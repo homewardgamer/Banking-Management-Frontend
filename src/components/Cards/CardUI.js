120fa7f46a6ecc970651237a24216c521c227434
Shashwat <shashwatmittal.india@gmail.com>
2022-04-28 18:19:15 +0530

    updating build and improved cus cards

--- src/components/Cards/CardUI.js
+++ src/components/Cards/CardUI.js
@@ -8,18 +8,10 @@ const CardsUI = props => {
         <navbar/>
    <div id="fed" className=" text">
    <div className="overflow" style={{borderTopLeftRadius:"1em", borderTopRightRadius:"1em"}}>
-       <img src={props.imgsrc} alt="cam" className="card-img-top" />
+       <img src={props.imgsrc} alt="cam" className="card-img-top" width="240px" height="240px"/>
    </div>
    <div className="card-body-small text-dark">
        <h4 className="card-title-small " >{props.title}</h4>
-       <p className="card-text " style={{color: 'white'}}>
-           {/* lorem epsum kkjjedhjdkehkehkehekhdkgdjgd ejgejkegkjehkejlelel dfjfhmh hgkfkg, gfkbbj jhfitoihk fyiuto8ln h7itig,j gukfkliyyftd ghli,jb fjyfhjmh oufjdgrsxcm dgcmv,luiu gdhtdrsh fiyfk */}
-24
-           {/* {props.content} */}
-       </p>
-       <p className="card-text " style={{color: 'white'}}>
-           $5604
-       </p>
    </div>
 
    </div>
--- src/components/Cards/CardsCus.js
+++ src/components/Cards/CardsCus.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Card from "./CardUI";
 import Card2 from "./Card2UI";
+import Verification from "../../img/verification.png"
+import MoneyTransfer from "../../img/moneytransfer.png"
+import BillPayment from "../../img/billpayment.png"
+import AccountStatement from "../../img/accountstatement.png"
 
 
 class Cards extends Component {
@@ -27,23 +31,27 @@ class Cards extends Component {
             </div>
             <div className="col-md-3 col-sm-12 ss" id="sec">
               <Card
-                imgsrc="https://images.pexels.com/photos/64057/pexels-photo-64057.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                title="INVITED TALKS"
+                imgsrc={Verification}
+                title="Verification"
                 content="The inviter generally sends the invitee a personal note or gives them a call, asks if they’d like to talk at a particular event, and relays details about the event."
               />
             </div>
             <div className="col-md-3 col-sm-12 ss" id="first">
               <Card
-                imgsrc="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                title="RESEARCH"
-                content="A poster presentation is a way to communicate your research or your understanding of a topic in a concise and visually engaging format."
+                imgsrc={AccountStatement}
+                title="Account Statement"
               />
             </div>
             <div className="col-md-3 col-sm-12 ss" id="sec">
               <Card
-                imgsrc="https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                title="POSTER PRESENTATION"
-                content="The creation of new knowledge and/or the use of existing knowledge in a new and creative way so as to generate new concepts, methodologies and understandings."
+                imgsrc={MoneyTransfer}
+                title="Money Transfer"
+              />
+            </div>
+            <div className="col-md-3 col-sm-12 ss" id="sec">
+              <Card
+                imgsrc={BillPayment}
+                title="Bill Payment"
               />
             </div>
           </div>
