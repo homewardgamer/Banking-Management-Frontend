9d09c95ca0969c41dd3afec90b0baa81d1b684c3
aashita chouhan <iit2020124@iiita.ac.in>
2022-04-28 19:17:34 +0530

    Modified Admin Cards

--- src/components/Cards/CardsAdmin.js
+++ src/components/Cards/CardsAdmin.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import Card from "./CardUI";
 import Card2 from "./Card2UI";
-
+import deposit from "../../img/deposit.png"
+import withdraw from "../../img/withdraw.png"
+import addBranch from "../../img/addBranch.png"
+import CustD from "../../img/CustomerDetails.png"
+import editCustInfo from "../../img/editCustInfo.png"
+import addEmployee from "../../img/addEmployee.png"
+import { Link } from "react-router-dom";
 
 class Cards extends Component {
   render() {
@@ -20,31 +26,57 @@ class Cards extends Component {
 
             <div className="col-md-12 col-sm-12 ff" id="first">
               <Card2
-                imgsrc="https://images.unsplash.com/photo-1560523159-6b681a1e1852?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                title="ADMIN SESSION"
-                content="A Conference or convocation and outlined the themes to be covered in ways that got the participants talking and thinking along the right lines."
+                title="Customer Name"
               />
             </div>
-            <div className="col-md-3 col-sm-12 ss" id="sec">
+            
+            <div className="col-md-2 col-sm-12 ss" id="first">
+            <Link to="/account-statement">
               <Card
-                imgsrc="https://images.pexels.com/photos/64057/pexels-photo-64057.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                title="INVITED TALKS"
-                content="The inviter generally sends the invitee a personal note or gives them a call, asks if they’d like to talk at a particular event, and relays details about the event."
+                imgsrc={deposit}
+                title="DEPOSIT MONEY"
               />
+              </Link>
             </div>
-            <div className="col-md-3 col-sm-12 ss" id="first">
+            <div className="col-md-2 col-sm-12 ss" id="sec">
+            <Link to="/transfer">
               <Card
-                imgsrc="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                title="RESEARCH"
-                content="A poster presentation is a way to communicate your research or your understanding of a topic in a concise and visually engaging format."
+                imgsrc={withdraw}
+                title="WITHDRAW MONEY"
               />
+              </Link>
             </div>
-            <div className="col-md-3 col-sm-12 ss" id="sec">
+            <div className="col-md-2 col-sm-12 ss" id="first">
+            <Link to="/bill">
               <Card
-                imgsrc="https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                title="POSTER PRESENTATION"
-                content="The creation of new knowledge and/or the use of existing knowledge in a new and creative way so as to generate new concepts, methodologies and understandings."
+                imgsrc={editCustInfo}
+                title="EDIT INFO"
               />
+              </Link>
+            </div>
+            <div className="col-md-2 col-sm-12 ss" id="sec">
+            <Link to="/block-account">
+              <Card
+                imgsrc={addEmployee}
+                title="ADD EMPLOYEE"
+              />
+              </Link>
+            </div>
+            <div className="col-md-2 col-sm-12 ss" id="first">
+            <Link to="/change-pwd">
+              <Card
+                imgsrc={addBranch}
+                title="ADD BRANCH"
+              />
+              </Link>
+            </div>
+            <div className="col-md-2 col-sm-12 ss" id="first">
+            <Link to="/change-pwd">
+              <Card
+                imgsrc={CustD}
+                title="CUSTOMER DETAILS"
+              />
+              </Link>
             </div>
           </div>
         </div>
