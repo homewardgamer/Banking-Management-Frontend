375fd1296cd147807a0b3d1a7685f600976a905a
aashita chouhan <iit2020124@iiita.ac.in>
2022-04-30 01:16:31 +0530

    Integrated backend to changePwd page

--- src/components/changepwd.js
+++ src/components/changepwd.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from 'react'
+import Axios from "axios";
 import NavbarAdmin from "./navbaradmin";
 
 function ChangePassword() {
+  var [old_password, setOldPassword] = useState("");
+  var [new_password, setNewPassword] = useState("");
+  var [new_password_confirm, setConfirmPass] = useState("");
+
+  function handleOldPassChange(event) {
+    const value = event.target.value;
+    setOldPassword(value);
+  }
+  function handleNewPassChange(event) {
+    const value = event.target.value;
+    setNewPassword(value);
+  }
+  function handleConfirmPassChange(event) {
+    const value = event.target.value;
+    setConfirmPass(value);
+  }
+
+  function changePwdApi(event) {
+    const url = "http://localhost:8000/api/user/changepassword";
+    const data = {old_password: old_password,new_password: new_password,new_password_confirm: new_password_confirm};
+    const token = localStorage.getItem('token');
+    Axios.post(url,{headers: {Authorization: `Token ${token}`}}, data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+
+
   return (
     <div>
       <NavbarAdmin />
       <div className="changepwd auth-wrapper">
         <form className="auth-inner">
           <h3>Change Password</h3>
-          <div className="mb-3">
-            {/* <label>Account Number</label> */}
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Enter Account Number"
-            />
-          </div>
 
           <div className="mb-3">
             {/* <label>Old Password</label> */}
@@ -23,6 +48,7 @@ function ChangePassword() {
               type="password"
               className="form-control"
               placeholder="Enter Old Password"
+              onChange={handleOldPassChange}
             />
           </div>
           <div className="mb-3">
@@ -31,9 +57,20 @@ function ChangePassword() {
               type="password"
               className="form-control"
               placeholder="Enter New Password"
+              onChange={handleNewPassChange}
             />
           </div>
 
+          <div className="mb-3">
+            {/* <label>New Password</label> */}
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm New Password"
+              onChange={handleConfirmPassChange}
+            />
+          </div>
+npm start
           <div className="d-grid">
             <button type="submit" className="btn btn-primary">
               Update Password
