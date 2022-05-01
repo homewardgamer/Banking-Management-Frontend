9b8609db71de648ccfa0e47956aabf61c685e99c
Aditya Verma <adityarubbers.av@gmail.com>
2022-05-01 19:16:44 +0530

    Added Account Information page

--- src/components/customer/seeaccountinfo.js
+++ src/components/customer/seeaccountinfo.js
@@ -1,59 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import NavbarAdmin from '../navbaradmin'
+import NavbarAdmin from "../navbaradmin";
+import { format } from "date-fns";
 
 // account_detail_by_id
-function SeeAccount(){
-var [s_account, setAccountNumber] = useState("");
-
-  function handleAccountNumChange(event) {
-    const value = event.target.value;
-    setAccountNumber(value);
-  }
-  function seeAccountApi(event) {
-    const url = `http://localhost:8000/api/account/view/${s_account}`;
-    const token = localStorage.getItem('token');
-    Axios.get(url,{headers: {Authorization: `Token ${token}`}})
+function SeeAccount() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:8000/api/account/list`;
+    const token = localStorage.getItem("token");
+    Axios.get(url, { headers: { Authorization: `Token ${token}` } })
       .then((res) => {
-        console.log(res.data);
+        setData(res.data);
       })
       .catch((err) => {
         alert(err);
       });
-  }
-  const name = localStorage.getItem('is_customer');
-  if (name=="true")
-  {
+  }, []);
+
+  const name = localStorage.getItem("is_customer");
+  if (name === "true") {
     return (
       <div>
-      <NavbarAdmin/>
-    <div className="auth-wrapper">
-      <form className="auth-inner">
-        <h3>See Account Information</h3>
-
-        <div className="mb-3">
-          {/* <label>Account Number</label> */}
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Account Number"
-            onChange={handleAccountNumChange}
-          />
+        <NavbarAdmin />
+        <div className="auth-wrapper">
+          <h1 style={{ margin: "2%" }}>Account(s) Information</h1>
+          <div class="wrapper">
+            <table class="table  table-hover">
+              <thead>
+                <tr className="table-dark">
+                  <th scope="col">Account ID</th>
+                  <th scope="col">Account Type</th>
+                  <th scope="col">Balance</th>
+                  <th scope="col">Created At</th>
+                  <th scope="col">Updated At</th>
+                  <th scope="col">Disabled</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <th scope="row">{val.account_id}</th>
+                      <td>{val.account_type}</td>
+                      <td>{val.balance}</td>
+                      <td>{format(new Date(val.created_at), "dd-MM-yyy")}</td>
+                      <td>{format(new Date(val.updated_at), "dd-MM-yyy")}</td>
+                      <td>{val.disabled ? "Yes" : "No"}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary" onClick={seeAccountApi}>
-            See Account Information
-          </button>
-        </div>
-        
-      </form>
       </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1 style={{ margin: "2rem" }}>Not Authorized.</h1>
       </div>
-    )}  else{
-      return <div>
-        <h1 style={{margin: "2rem"}}>Not Authorized.</h1>
-      </div>
-    }
-  }  
-  export default SeeAccount;
+    );
+  }
+}
+export default SeeAccount;
