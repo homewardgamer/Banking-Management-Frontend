a3b38b473a56f49616539ff112b7a9556d3c6e17
Aditya Verma <adityarubbers.av@gmail.com>
2022-05-03 16:44:43 +0530

    Added See branch customer and see customer account routes

--- src/components/admin/branchcustomer.js
+++ src/components/admin/branchcustomer.js
@@ -1,8 +1,10 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Axios from "axios";
 import NavbarAdmin from "../navbaradmin";
+import { format } from "date-fns";
 function NewBranch() {
   var [branch_id, setBranchId] = useState("");
+  const [data, setData] = useState([]);
 
   function handleBranchIdChange(event) {
     const value = event.target.value;
@@ -10,55 +12,102 @@ function NewBranch() {
   }
 
   function BranchCusApi(event) {
+    event.preventDefault();
     const url = `http://localhost:8000/api/branch/${branch_id}/customers`;
     const token = localStorage.getItem("token");
     Axios.get(url, { headers: { Authorization: `Token ${token}` } })
       .then((res) => {
         console.log(res.data);
+        setData(res.data);
+        if (res.data.length === 0) {
+          alert("No Customers found from this branch!");
+        } else {
+        }
       })
       .catch((err) => {
         alert(err);
       });
   }
-
-  const name = localStorage.getItem('is_admin');
-  if (name=="true")
-  {
+  function InputBox() {
     return (
-    <div>
-      <NavbarAdmin />
-      <div className="changepwd auth-wrapper">
-        <form className="auth-inner">
-          <h3>Branch Customers</h3>
+      <div>
+        <NavbarAdmin />
+        {data.length === 0 ? "FUCK" : "NO"}
+        <div className="changepwd auth-wrapper">
+          <form className="auth-inner">
+            <h3>Branch Customers</h3>
 
-          <div className="mb-3">
-            {/* <label>Branch Id</label> */}
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Enter Branch Id"
-              onChange={handleBranchIdChange}
-            />
-          </div>
+            <div className="mb-3">
+              {/* <label>Branch Id</label> */}
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Enter Branch Id"
+                onChange={handleBranchIdChange}
+              />
+            </div>
 
-          <div className="d-grid">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={BranchCusApi}
-            >
-              Get Customer Details
-            </button>
+            <div className="d-grid">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={BranchCusApi}
+              >
+                Get Customer Details
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  function Table() {
+    return (
+      <div>
+        <NavbarAdmin />
+        <div className="auth-wrapper">
+          <h1 style={{ margin: "2%" }}>Account(s) Information</h1>
+          <div class="wrapper">
+            <table class="table  table-hover">
+              <thead>
+                <tr className="table-dark">
+                  <th scope="col">User ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Username</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Date of Birth</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <th scope="row">{val.id}</th>
+                      <td>{val.first_name + " " + val.last_name}</td>
+                      <td>{val.username}</td>
+                      <td>{val.address}</td>
+                      <td>{val.dob}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-  );
-    } else{
-      return <div>
-        <h1 style={{margin: "2rem"}}>Not Authorized.</h1>
+    );
+  }
+
+  const name = localStorage.getItem("is_admin");
+  if (name === "true") {
+    return <div>{data.length === 0 ? <InputBox /> : <Table />}</div>;
+  } else {
+    return (
+      <div>
+        <h1 style={{ margin: "2rem" }}>Not Authorized.</h1>
       </div>
-    }
+    );
+  }
 }
 
 export default NewBranch;
--- src/components/admin/seecusaccounts.js
+++ src/components/admin/seecusaccounts.js
@@ -1,64 +1,69 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import NavbarAdmin from '../navbaradmin'
-
+import NavbarAdmin from "../navbaradmin";
+import { format } from "date-fns";
 // account_detail_by_id
-function SeeCusAccounts(){
-var [id, setCusId] = useState("");
+function SeeCusAccounts() {
+  const [data, setData] = useState([]);
 
-  function handleAccountNumChange(event) {
-    const value = event.target.value;
-    setCusId(value);
-  }
-  function SeeCusAccountsApi(event) {
-    const url = `http://localhost:8000/api/account/list`;
-    const token = localStorage.getItem('token');
-    const data= {id:id};
-    event.preventDefault();
-    Axios.get(url,data,{headers: {Authorization: `Token ${token}`}})
+  useEffect(() => {
+    const url = `http://localhost:8000/api/account/view/all`;
+    const token = localStorage.getItem("token");
+    Axios.get(url, { headers: { Authorization: `Token ${token}` } }, data)
       .then((res) => {
         console.log(res.data);
-        alert("Fetched Account Successfully!!");
-        window.location.href = "/admin";
+        setData(res.data);
       })
       .catch((err) => {
+        console.log(err);
         alert(err);
       });
-  }
-  const name = localStorage.getItem('is_admin');
-  if (name=="true")
-  {
+  }, []);
+  const name = localStorage.getItem("is_admin");
+  if (name === "true") {
     return (
       <div>
-      <NavbarAdmin/>
-    <div className="auth-wrapper">
-      <form className="auth-inner">
-        <h3>See Customer Accounts</h3>
-
-        <div className="mb-3">
-          {/* <label>Account Number</label> */}
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Customer Id"
-            onChange={handleAccountNumChange}
-          />
+        <NavbarAdmin />
+        <div className="auth-wrapper">
+          <h1 style={{ margin: "2%" }}>Account(s) Information</h1>
+          <div class="wrapper">
+            <table class="table  table-hover">
+              <thead>
+                <tr className="table-dark">
+                  <th scope="col">Account ID</th>
+                  <th scope="col">Account Type</th>
+                  <th scope="col">Balance</th>
+                  <th scope="col">Created At</th>
+                  <th scope="col">Updated At</th>
+                  <th scope="col">Disabled</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <th scope="row">{val.account_id}</th>
+                      <td>{val.account_type}</td>
+                      <td>{val.balance}</td>
+                      <td>{format(new Date(val.created_at), "dd-MM-yyy")}</td>
+                      <td>{format(new Date(val.updated_at), "dd-MM-yyy")}</td>
+                      <td>{val.disabled ? "Yes" : "No"}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary" onClick={SeeCusAccountsApi}>
-            See Customer Accounts
-          </button>
-        </div>
-        
-      </form>
       </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1 style={{ margin: "2rem" }}>Not Authorized.</h1>
       </div>
-    )}  else{
-      return <div>
-        <h1 style={{margin: "2rem"}}>Not Authorized.</h1>
-      </div>
-    }
+    );
   }
+}
 
-  export default SeeCusAccounts;
+export default SeeCusAccounts;
